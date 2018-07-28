04e2b3c13414ab3dcef7008be9c803591cf074ce
Pawel Lulinski <pawel.lulinski@laboratorium.ee>
2018-07-28 18:43:13 +0200

    additional info about photo, api config as object

--- containers/App.js
+++ containers/App.js
@@ -5,31 +5,48 @@ import PropTypes from "prop-types";
 
 const apiKey = "7e35ea37110806c7b4fe6267662f21f4";
 const tags = "dog";
-const jsonRequest = "http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=" + apiKey + "&tags=" + tags + "&format=json&nojsoncallback=true";
+const apiConfigParams = {
+    method: "flickr.photos.search",
+    api_key: apiKey,
+    tags: tags,
+    format: "json",
+    nojsoncallback: "true",
+    extras: "description, owner_name, date_taken"
+};
+const apiLink = "http://api.flickr.com/services/rest/";
+const apiParams = Object.keys(apiConfigParams).map(key => key + '=' + apiConfigParams[key]).join('&');
+const jsonRequest = apiLink + "?" + apiParams;
+const PhotoCard = (props) => {
+    const checkIfEmpty = text => {
+        if (typeof text === "undefined" || text === "") {
+            return '-'
+        } else {
+            return text;
+        }
+    };
 
-const DogCard = (props) => {
-    let title = props.title;
-    if (typeof title === "undefined" || title === "") {
-        title = '-'
-    }
     return (
-        <div className="dog-card" style={{margin: "1rem"}}>
+        <div className="photo-card">
             <img src={props.url} />
-            <div style={{marginLeft: "20px"}}>
-                <div><strong>Title: </strong>{title}</div>
-                <div><strong>Author ID: </strong><a href={"https://www.flickr.com/photos/" + props.owner + "/"}>{props.owner}</a></div>
+            <div>
+                <p><strong>Title: </strong><span className="photo-title">{checkIfEmpty(props.title)}</span></p>
+                <p><strong>Author: </strong><a href={"https://www.flickr.com/photos/" + props.ownerId } title="author's other pictures">{props.ownerName}</a></p>
+                <p><strong>Description: </strong>{checkIfEmpty(props.description)}</p>
+                <p><strong>Taken: </strong>{props.dateTaken}</p>
             </div>
         </div>
     );
 };
 
-DogCard.propTypes = {
+PhotoCard.propTypes = {
     title: PropTypes.string,
     url: PropTypes.string,
-    owner: PropTypes.string,
+    description: PropTypes.string,
+    ownerId: PropTypes.string,
+    ownerName: PropTypes.string,
 };
 
-class DogList extends React.Component {
+class PhotoList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -64,10 +81,14 @@ class DogList extends React.Component {
         } else if (!isLoaded) {
             return <div>loading...</div>;
         } else {
-            const listItems = items.photos.photo.map((item,i) => {
-                return <DogCard owner={item.owner} key={i}
-                    url={"https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg"}
-                    title={item.title} />;
+            const listItems = items.photos.photo.map((photo,i) => {
+                return <PhotoCard key={i}
+                    dateTaken={photo.datetaken}
+                    description={photo.description._content}
+                    ownerName={photo.ownername}
+                    ownerId={photo.owner}
+                    url={"https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg"}
+                    title={photo.title} />;
             });
 
             return <div>{listItems} </div>;
@@ -77,6 +98,11 @@ class DogList extends React.Component {
 
 export default class App extends Component {
     render () {
-        return <DogList />;
+        return (
+            <div className="photo-gallery">
+                <h1>Photos tagged as: {tags}</h1>
+                <PhotoList />
+            </div>
+        );
     }
 }
