229024b64b67a628bbfc0bf89ca9d4baffa60fb0
Pawel Lulinski <pawel.lulinski@laboratorium.ee>
2018-07-28 16:53:42 +0200

    dogs pictures visible on the page

--- containers/App.js
+++ containers/App.js
@@ -1,34 +1,79 @@
+/* eslint-disable */
 import React, {Component} from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
+const apiKey = "7e35ea37110806c7b4fe6267662f21f4";
+const tags = "dog";
+const jsonRequest = "http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=" + apiKey + "&tags=" + tags + "&format=json&nojsoncallback=true";
+
 const DogCard = (props) => {
+    let title = props.title;
+    if (typeof title === "undefined" || title === "") {
+        title = '-'
+    }
     return (
         <div className="dog-card" style={{margin: "1rem"}}>
-            <img width="90" src={props.url} />
+            <img src={props.url} />
             <div style={{marginLeft: "20px"}}>
-                <div style={{fontWeight: "bold"}}>{props.name}</div>
-                <div>{props.owner}</div>
+                <div><strong>Title: </strong>{title}</div>
+                <div><strong>Author ID: </strong><a href={"https://www.flickr.com/photos/" + props.owner + "/"}>{props.owner}</a></div>
             </div>
         </div>
     );
 };
 
 DogCard.propTypes = {
-    name: PropTypes.string,
+    title: PropTypes.string,
     url: PropTypes.string,
     owner: PropTypes.string,
 };
 
-const DogList = () => {
-    return (
-        <div>
-            <DogCard name="Fafik"
-                url="http://placehold.it/90"
-                owner="Paul"
-            />
-        </div>
-    );
-};
+class DogList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(jsonRequest)
+            .then(
+                result => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.data
+                    });
+                    console.log(result.data);
+                }, error => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                });
+    }
+
+    render() {
+        const {error, isLoaded, items} = this.state;
+
+        if (error) {
+            return <div>Error: {error} </div>;
+        } else if (!isLoaded) {
+            return <div>loading...</div>;
+        } else {
+            const listItems = items.photos.photo.map((item,i) => {
+                return <DogCard owner={item.owner} key={i}
+                    url={"https://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + ".jpg"}
+                    title={item.title} />;
+            });
+
+            return <div>{listItems} </div>;
+        }
+    }
+}
 
 export default class App extends Component {
     render () {
